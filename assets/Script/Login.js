4a76f6960abf29ede89ba0f9c4f1bd67dc06bd75
honlessy <443048317@qq.com>
2017-08-11 20:46:18 +0800

    创建了LOF项目并且做到了登录界面，但是登录框的验证还没有实现

---
+++ assets/Script/Login.js
@@ -0,0 +1,40 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+        errorPrefab:{
+            type:cc.Prefab,
+            default:null,
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+    touchToNextScene(){
+        var editname = this.node.getChildByName("editname");
+        var editpwd = this.node.getChildByName("editpwd");
+        
+        if(editname.string ===""||editpwd.string === ""){
+            var error = cc.instantiate(this.errorPrefab);
+            this.node.addChild(error);
+            error.setPosition = cc.p(0,0);
+        }
+        cc.director.loadScene('ChooseCharactor');
+    },
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
